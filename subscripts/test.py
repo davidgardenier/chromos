4487f37fce4941d267f93f794bd6c470c3d6babb
David Gardenier <davidgardenier@gmail.com>
2015-11-11 15:42:05 +0100

    Checking for multiple filetypes per obsid

--- subscripts/test.py
+++ subscripts/test.py
@@ -1,5 +1,6 @@
 import json
 import os
+import glob
 
 PATH = '/scratch/david/master_project/full_data'
 OBJECT_NAME = 'aquila'
@@ -7,7 +8,16 @@ PRINT_OUTPUT = True
 
 os.chdir(PATH)
 
+# Find all obsids
+initial_obsids = sorted([r.split('/')[-1] for r in glob.glob('./*/?????-??-??-*')])
+# Getting rid of obsids with letters at the end
+obsids = [o for o in initial_obsids if o[-1].isdigit()]
+
 with open('./info_on_files.json', 'r') as info:
     d = json.load(info)
 
-print json.dumps(d, sort_keys=True, indent=4)
+n = 0
+o = 0
+for obsid in d:
+    if 'event' not in d[obsid].keys() and 'goodxenon' not in d[obsid].keys():
+        print obsid
