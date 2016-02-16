1bc6b3d67a9a6ff011fe623c3a95b7dfce1d3213
David Gardenier <davidgardenier@gmail.com>
2016-02-16 14:53:14 +0100

    Initializing new pipeline

---
+++ main_pipeline.py
@@ -0,0 +1,7 @@
+# Master Project - Scripts for working with RXTE-data
+# Written by David Gardenier, davidgardenier@gmail.com, 2015-2016
+
+from subscripts.paths import *
+from subscripts.download_data import *
+
+download()
---
+++ paths.py
@@ -0,0 +1,12 @@
+import os
+
+selection = 'test'
+subscripts = os.getcwd() + '/subscripts/'
+
+root = '/scratch/david/master_project/'
+
+data = root + selection + '/'
+data_info = data + 'info/'
+
+obsid_lists = root + 'obsid_lists/'
+obsid_list = obsid_lists + selection + '/'
