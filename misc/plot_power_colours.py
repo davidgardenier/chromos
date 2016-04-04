a8dee6f9c382f38858e5fe90d9c4ad841a1304ee
David Gardenier <davidgardenier@gmail.com>
2016-04-04 13:44:55 +0200

    Ensure only one power colour per obsid is displayed

--- misc/plot_power_colours.py
+++ misc/plot_power_colours.py
@@ -10,13 +10,48 @@ def path(o):
     return '/scratch/david/master_project/' + o + '/info/database.csv'
 
 
+def findbestres(res):
+    '''Find the smallest resolution from a list of resolutions'''
+
+    # Split resolutions into values and units
+    heads = []
+    tails = []
+    for s in res:
+        unit = s.strip('0123456789')
+        num = s[:-len(unit)]
+        heads.append(num)
+        tails.append(unit)
+
+    # Sort by unit, then by value
+    unitorder = ['ms','us','s']
+    for u in unitorder:
+        if u in tails:
+            indices = [i for i, x in enumerate(tails) if x==u]
+            sameunits = [heads[i] for i in indices]
+            sortvalues = sorted(sameunits)
+            return sortvalues[0]+u
+
+
+def bestdata(df):
+    '''Select the data with best mode and resolution'''
+    ordering = ['goodxenon','event','binned','std2']
+    for mode in ordering:
+        if mode in df.modes.values:
+            df = df[df.modes==mode]
+            if df.shape[0] > 1:
+                bestres = findbestres(df.resolutions.values)
+                return df[df.resolutions==bestres].iloc[0]
+            else:
+                return df.iloc[0]
+
+
 objects = ['4u_1705_m44',
            'xte_J1808_369',
            'cir_x1',
            'cyg_x2',
            'EXO_0748_676',
            'HJ1900d1_2455',
-           'sco_x1', # Still runnning
+           'sco_x1',
            '4U_1728_34',
            'J1701_462',
            'aquila_X1',
@@ -32,7 +67,8 @@ for o in objects:
     print '=======================\n', db.apply(pd.Series.nunique)
 
     db = db[(db.pc1.notnull() & db.lt3sigma==True)]
-    db = db.drop_duplicates(['obsids','pc1'])
+    db = db.groupby('obsids').apply(bestdata)
+    print '%d data points for %s' %(len(db),o)
     x = db.pc1.values
     y = db.pc2.values
     xerror = db.pc1_err.values
@@ -48,7 +84,7 @@ for o in objects:
     plt.title('Power Colours')
     plt.legend()
     #plt.show()
-    #plt.savefig('/scratch/david/master_project/plots/pc_' + o + '.png')
-    #plt.clf()
+    plt.savefig('/scratch/david/master_project/plots/pc_' + o + '.png')
+    plt.clf()
 
-plt.show()
+# plt.show()
